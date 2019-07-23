a915a812206b0e4ae7b64335302de5e72582336a
kamonkarn <kamonkarnrueangsang@gmail.com>
2019-07-23 10:09:35 +0700

    Initial commit

---
+++ src/lab1/Input.java
@@ -0,0 +1,24 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("What is your name?: " );
+        String name = scanner1.nextLine();
+        System.out.println(name);
+        System.out.print("กรุณากรอกอายุ");
+        int อายุ = scanner.nextInt();
+        System.out.print(อายุ);
+
+        System.out.print("E-mail? : ");
+        String E = scanner.nextLine();
+        System.out.print(E);
+
+    }//main
+
+
+
+}//class
---
+++ src/lab1/intro.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class intro {
+    public static void main(String[] args) {
+        System.out.println("Hello, OOP 2019.");
+        System.out.print("MIT & IS,\n");
+        System.out.print("\tMIT & IS,");
+
+
+        //integer
+        int x;
+        x = 10;
+        int y = 20;
+        System.out.print(x);
+        System.out.print(y);
+        System.out.println(x + y);
+        System.out.println("The summation of x and y = " + (x + y));
+
+
+    }//main
+}//class
+
